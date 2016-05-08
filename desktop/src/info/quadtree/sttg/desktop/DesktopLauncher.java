4e1925669b501b47f5935f3950c724417ba96e4c
Eric Brown <quadtree@gmail.com>
2016-05-07 17:20:23 -0700

    Start

---
+++ desktop/src/info/quadtree/sttg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package info.quadtree.sttg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.quadtree.sttg.STTG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new STTG(), config);
+	}
+}
